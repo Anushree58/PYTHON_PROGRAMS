7505e00dfa9ce9374413f297305b91f9e74be0e0
Anushree58 <anushreeanu0220@gmail.com>
2025-01-15 12:53:01 +0530

    Add files via upload

---
+++ common.py
@@ -0,0 +1,5 @@
+a=[1,2,3,4,5]
+b=[2,3,6,5,8]
+
+x=(set(a).intersection(b))
+print(x)
---
+++ sum&avg.py
@@ -0,0 +1,5 @@
+n=[1,3,5,8]
+sum=1+3+5+8
+avg=sum/len(n)
+print("sum of number is",sum)
+print("sum of average is",avg)
